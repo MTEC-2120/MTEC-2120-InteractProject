da8b2a9b5c853ae37b1aebf2df08e3c5e759e1b3
Sky Rolnick <skymeson@gmail.com>
2022-12-08 01:58:33 -0500

    Updated the Portal Links.

--- Assets/Kyle/Obstacle.cs
+++ Assets/Kyle/Obstacle.cs
@@ -9,13 +9,36 @@ public class Obstacle : MonoBehaviour
 {
     [SerializeField] private Transform Player;
     [SerializeField] private Transform SpawnPoint;
-    
-    void OnCollisionEnter (Collision coll)
-    { 
-    if (coll.gameObject.tag == "Player")
-        
+
+    void OnCollisionEnter(Collision coll)
     {
+        Debug.Log("Obstacle OnCollisionEnter");
+        if (coll.gameObject.tag == "Player")
+
+        {
+
+            Debug.Log("Respawn Player");
+
             Player.transform.position = SpawnPoint.transform.position;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        Debug.Log("Obstacle OnTriggerEnter");
+
+        if (other.gameObject.tag == "Player")
+
+        {
+            Debug.Log("Respawn Player " + SpawnPoint.transform.position);
+            Player.gameObject.GetComponent<CharacterController>().enabled = false;
+            Player.gameObject.SetActive(false); 
+            Player.transform.position = SpawnPoint.transform.position;
+            Player.gameObject.SetActive(true);
+            Player.gameObject.GetComponent<CharacterController>().enabled = true;
+
+
         }
     }
 
